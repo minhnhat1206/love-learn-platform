8204a7223470c6a610e1724920481c287de8c701
minhnhat <minhnhatnhc@gmail.com>
2026-02-16 09:13:17 +0700

    add test 1

--- lessons/testRegistry.ts
+++ lessons/testRegistry.ts
@@ -1,8 +1,10 @@
 import { TestContent } from '../types';
 import { Test1 } from '../tests/Test1';
+import { GrammarTest1 } from '../tests/GrammarTest1';
 
 export const AllTests: TestContent[] = [
     Test1,
+    GrammarTest1,
 ];
 
 export const getTestById = (id: string): TestContent | undefined => {
